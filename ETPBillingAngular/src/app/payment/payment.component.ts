f7abcbe5d3090b1fecc289bfab17cd8b95fed1e8
Giridhar Shenoy <giridharsh1@gmail.com>
2018-06-07 11:59:24 +0530

    credit card changes

--- ETPBillingAngular/src/app/payment/payment.component.ts
+++ ETPBillingAngular/src/app/payment/payment.component.ts
@@ -14,18 +14,18 @@ export class PaymentComponent implements OnInit {
   @Input() title?: string;
   objItem = [];
 
-  url_productInfo = 'http://110.173.181.78:6040/ETPStoreServiceV5.5_REST/rest/Service/GET_PRODUCT_INFO';
-  json_productInfo = './assets/data/getProductInfo.json';
+  //url_productInfo = 'http://110.173.181.78:6040/ETPStoreServiceV5.5_REST/rest/Service/GET_PRODUCT_INFO';
+  //json_productInfo = './assets/data/getProductInfo.json';
 
-  public itemNumber = '';
-  name = '';
-  itemType = '';
-  prodGroup = '';
-  salesPrice = '';
-  mode;
-  tmpIndex = -1;
-  currentIndex = -1;
-  currentItemObj = [];
+  // public itemNumber = '';
+  // name = '';
+  // itemType = '';
+  // prodGroup = '';
+  // salesPrice = '';
+   mode;
+  // tmpIndex = -1;
+  // currentIndex = -1;
+  // currentItemObj = [];
   amountObj :Object;
   grossAmt;
   cashAmt;
@@ -33,12 +33,21 @@ export class PaymentComponent implements OnInit {
   balanceAmt;
   received;
   cashAmount;
+  ccAmount;
+  ccNumber;
+  ccType;
+  ccEdc;
+  ccName;
+  ccValid;
+  ccApno;
 
+paymentArr = [];
   constructor(private commonService: CommonService, private sharedService: SharedService) {
     this.cashAmount = 0;
    this. cashAmt=0;
   this.creditAmt=0;
   this.balanceAmt=0;
+  this.ccAmount = 0;
    }
 
   ngOnInit() {
@@ -47,15 +56,43 @@ this.mode = 'CASH';
 
 
 this.sharedService.grossAmountValue.subscribe(grossAmount => {
-     // this.amountObj['gross']=grossAmount;
      this.grossAmt=grossAmount;
     });
   }
   acceptCash(){
-    this.cashAmt = this.cashAmount;
+    if(this.cashAmt +this.creditAmt >this.grossAmt){
+    }else{
+      let paymentObj = {
+        'amount':this.cashAmount,
+        'mode':'CASH'
+      }
+      this.paymentArr.push(paymentObj);
+       this.cashAmt = this.cashAmount;
     this.balanceAmt = this.grossAmt - this.cashAmt - this.creditAmt;
+    }
   }
-  show() {
+  acceptCreditCard(){
+  //  if(this.cashAmt +this.creditAmt >this.grossAmt){
+  // }else{
+   
+      let paymentObj = {
+        'amount':this.ccAmount,
+        'mode':'CREDITCARD',
+        'number':this.ccNumber,
+        'type':this.ccType,
+        'edcDetails':this.ccEdc,
+        'name':this.ccName,
+        'expiry':this.ccValid,
+        'apno':this.ccApno,
+        'agency':'Agency'
+      }
+      this.paymentArr.push(paymentObj);
+       this.creditAmt = this.ccAmount;
+    this.balanceAmt = this.grossAmt - this.cashAmt - this.creditAmt;
+    // }
+  } 
+  
+  show(){
     console.log('In show');
     console.log(this.childModalPayment);
     this.cancel();
@@ -68,11 +105,14 @@ this.sharedService.grossAmountValue.subscribe(grossAmount => {
 
   cancel() {
   }
-
-  selectRow(event, data, index) {
-    console.log(event.type);
-    this.currentIndex = index;
-    this.currentItemObj = data;
+  changeMode(mode){
+    this.mode = mode;
   }
+
+  // selectRow(event, data, index) {
+  //   console.log(event.type);
+  //   this.currentIndex = index;
+  //   this.currentItemObj = data;
+  // }
 
 }
